bf7671be27c0ab63a917fea9865f011b72cf7b4c
Gavin Morris <gavin@almostpurple.com>
2021-02-05 17:53:55 +0800

    update host name

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module Blog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.hosts << "setls-az.almostpurple.com"
+    config.hosts << "blog.almostpurple.com"
   end
 end

fbb6a42e7949cc8bc6cad23509b46c850aff50cb
Gavin Morris <gavin@almostpurple.com>
2021-02-06 17:28:58 +0800

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
-
+  constraints :subdomain /.*/ do
+    get "/articles", to: "articles#index"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

95e3b5cb68733c325d922f1246cc6533c6fe04dd
Gavin Morris <gavin@almostpurple.com>
2021-02-08 21:28:06 +0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  constraints :subdomain /.*/ do
-    get "/articles", to: "articles#index"
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/articles", to: "articles#index", constraints: { subdomain: /.*/ }
+
 end

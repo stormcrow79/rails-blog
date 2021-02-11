6941655d1e6521218c597e050f7c3ff1d99859b4
Gavin Morris <gavin@almostpurple.com>
2021-02-11 20:13:35 +0800

    add multiple databases

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  slug = request.subdomain.split('.')[0]
+  ActiveRecord::Base.connected_to(role: :writing, shard: slug.to_sym)
 end

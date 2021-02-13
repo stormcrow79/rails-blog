065abe502a6f801aec20cc335e58208fe8b41bd7
Gavin Morris <gavin@almostpurple.com>
2021-02-13 16:31:04 +0800

    move sharding to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    around_action :determine_database_connection
+
+    private
+    
+    def determine_database_connection
+        slug = request.subdomain.split('.')[0]
+        ActiveRecord::Base.connected_to(role: :writing, shard: slug.to_sym) do
+            yield
+        end
+    end  
 end
